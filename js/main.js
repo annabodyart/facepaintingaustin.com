5a3f10555ba1527c3ede382caf2857adb7a91c46
fpliger <fabio.pliger@gmail.com>
2017-04-14 15:37:13 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,81 @@
+$(document).ready(function(){
+
+
+	$("#portfolio-contant-active").mixItUp();
+
+
+	$("#testimonial-slider").owlCarousel({
+	    paginationSpeed : 500,
+	    singleItem:true,
+	    autoPlay: 3000,
+	});
+
+
+
+
+  $('#portos').slick({
+
+  centerMode: true,
+  centerPadding: '60px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+  });
+
+
+	$("#clients-logo").owlCarousel({
+		autoPlay: 3000,
+		items : 5,
+		itemsDesktop : [1199,5],
+		itemsDesktopSmall : [979,5],
+	});
+
+	$("#works-logo").owlCarousel({
+		autoPlay: 3000,
+		items : 5,
+		itemsDesktop : [1199,5],
+		itemsDesktopSmall : [979,5],
+	});
+
+
+	// google map
+		var map;
+		function initMap() {
+		  map = new google.maps.Map(document.getElementById('map'), {
+		    center: {lat: -34.397, lng: 150.644},
+		    zoom: 8
+		  });
+		}
+
+
+	// Counter
+
+	$('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
+
+
+});
+
+
+
+
